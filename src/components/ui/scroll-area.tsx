20a9a0a516fede48880d8d0dea70a7684a23b42f
Jorge Ramirez Palomino <500760@tepeyac.mx>
2025-05-30 23:56:33 +0000

    No las miro adentro de las tablas para ver todo adentro de ellas

--- src/components/ui/scroll-area.tsx
+++ src/components/ui/scroll-area.tsx
@@ -1,3 +1,4 @@
+
 "use client"
 
 import * as React from "react"
@@ -31,16 +32,16 @@ const ScrollBar = React.forwardRef<
     ref={ref}
     orientation={orientation}
     className={cn(
-      "flex touch-none select-none transition-colors",
+      "flex touch-none select-none transition-colors z-20", // Added z-20 for good measure
       orientation === "vertical" &&
-        "h-full w-2.5 border-l border-l-transparent p-[1px]",
+        "h-full w-2.5 border-l border-l-border bg-muted/30 p-[1px]", // Changed border to border-l-border, added bg-muted/30 to track
       orientation === "horizontal" &&
-        "h-2.5 flex-col border-t border-t-transparent p-[1px]",
+        "h-2.5 flex-col border-t border-t-border bg-muted/30 p-[1px]", // Changed border to border-t-border, added bg-muted/30 to track
       className
     )}
     {...props}
   >
-    <ScrollAreaPrimitive.ScrollAreaThumb className="relative flex-1 rounded-full bg-border" />
+    <ScrollAreaPrimitive.ScrollAreaThumb className="relative flex-1 rounded-full bg-primary opacity-75 hover:opacity-100" /> {/* Changed thumb to bg-primary and added opacity states */}
   </ScrollAreaPrimitive.ScrollAreaScrollbar>
 ))
 ScrollBar.displayName = ScrollAreaPrimitive.ScrollAreaScrollbar.displayName
